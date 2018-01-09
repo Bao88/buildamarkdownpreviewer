46bd59129c3d7190df488391ccd719f71540d921
Bao88 <bao88@live.no>
2018-01-09 13:22:44 +0100

    no need to think about "Enter"
    asdf

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./css/main.css";
 var marked = require("marked");
+const ReactMarkdown = require("react-markdown");
 
 class Marked extends React.Component {
     convertTextToMarkdown(mark){
         // alert(mark);
         var markup = marked(mark, {sanitize: true});
-        
         return { __html: markup };
     }
     render(){
@@ -18,41 +18,17 @@ class Marked extends React.Component {
 
 class Main extends React.Component {
     handleChange = (event) => {
-        // alert(marked("I am using __markdown__"));
-        // event.key;
-        // alert();
         var preview = { __html: marked(event.target.value, {sanitize:true})};
-        this.setState({str: <span dangerouslySetInnerHTML={preview}/>});
+        this.setState({str: <p dangerouslySetInnerHTML={preview}/>});
     }
 
-    handleKeyPress = (event) => {
-        // alert(event.key);
-        if(event.key === "Enter"){
-            console.log(this.state.str);
-            var preview = { __html: marked("hello", {sanitize:true})};
-            this.setState({str: <span dangerouslySetInnerHTML={preview}/>});
-            console.log(this.state.str);
-            // var preview = { __html: marked(this.state.str + "\n", {sanitize:true}) + " Hello"};
-            
-            // this.setState({str: <span dangerouslySetInnerHTML={preview}/>});
-
-            // alert("Enter");
-            // this.setState({str: this.state.str + });
-            // alert(this.state.str);
-            // document.getElementById("previewer").appendChild(document.createElement("br"));
-            // document.getElementById("previewer").appendChild(document.createElement("br"));
-        }
-        // this.setState({str: event.key});
-    }
     state = {str: null};
-
     render(){
         return (
             <div>
-                <textarea id="marker" className="split" onChange={this.handleChange} onKeyPress={this.handleKeyPress}></textarea>
+                {<textarea id="marker" className="split" onChange={this.handleChange}></textarea>}
                 <span id="previewer" className="split">
                     {this.state.str}
-                    {/* <Marked className="split" mark={this.state.str}/> */}
                 </span>
             </div>
         );
